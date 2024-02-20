e3a8089ef07e6dc0549cb20e28e8adbefa4726a0
Rishik Sarkar <rishiksarkar123@gmail.com>
2024-02-20 13:06:31 -0500

    added pet details tab

--- components/Pet.jsx
+++ components/Pet.jsx
@@ -3,8 +3,29 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { FaCaretLeft, FaCaretRight } from 'react-icons/fa';
+import { MdEdit } from "react-icons/md";
 
 export default function Pet() {
+  const [petName, setPetName] = useState('Poofy');
+  const [editing, setEditing] = useState(false);
+  const [tempPetName, setTempPetName] = useState(petName);
+
+  const handleNameChange = (event) => {
+    setTempPetName(event.target.value);
+  };
+
+  const handleBlur = () => {
+    setPetName(tempPetName);
+    setEditing(false);
+  };
+
+  const toggleEditMode = () => {
+    setEditing(true);
+  };
+
+  const [currLevel, setCurrLevel] = useState(1);
+  const [levelProgress, setLevelProgress] = useState(0);
+
   const [frameIndex, setFrameIndex] = useState(0);
   const [idleCount, setIdleCount] = useState(0);
   const [currentAnimation, setCurrentAnimation] = useState('idle');
@@ -16,6 +37,8 @@ export default function Pet() {
   const [currFood, setCurrFood] = useState("onigiri");
   const [foodOptions, setFoodOptions] = useState(["onigiri", "maki", "nigiri"]);
   const [foodIndex, setFoodIndex] = useState(0);
+
+  const [favoriteFood, setFavoriteFood] = useState("nigiri");
 
   function generateSequence(basePath, count) {
     return Array.from({ length: count }, (_, i) => `${basePath}${i + 1}.png`);
@@ -80,6 +103,8 @@ export default function Pet() {
       setFrameIndex(0); // Start the eat animation from the first frame
       setShowFood(true);
 
+      increaseLevel(currFood);
+
       setTimeout(() => {
         setCurrentAnimation('idle');
         setFrameIndex(0); // Return to the idle animation at the first frame
@@ -96,35 +121,91 @@ export default function Pet() {
     });
   };
 
+  const increaseLevel = (food) => {
+    if (food === favoriteFood) {
+      setLevelProgress(levelProgress + 20);
+    }
+    else {
+      setLevelProgress(levelProgress + 10);
+    }
+
+    if (food === favoriteFood && levelProgress >= 80) {
+      setLevelProgress(levelProgress - 80);
+      setCurrLevel(currLevel + 1);
+    }
+    else if (levelProgress >= 90) {
+      setLevelProgress(levelProgress - 90);
+      setCurrLevel(currLevel + 1);
+    }
+  };
+
   return (
     <div id='pet' className='bg-white w-full h-screen font-mono select-none'>
       <div className="w-full h-full flex flex-col items-center justify-center text-center">
-        <div className='border-8 border-black'>
-          <div className="w-[50vw] h-[50vh] flex bg-white items-end justify-center">
+        <div className='w-full h-[60vh] items-center justify-center text-center grid grid-cols-4 gap-8'>
 
-            <div className='z-10 relative w-full h-full'>
-              <Image src={"/assets/backgrounds/" + currBg} layout="fill" />
-              <div className='z-20 absolute bottom-1 left-1/2 transform -translate-x-1/2'>
-                <Image src={animations[currentAnimation].sequence[frameIndex]} alt="Pet" width={200} height={200} unoptimized={true} />
+          <div className='h-full col-span-1 bg-black border-8 border-black ml-8 items-center justify-center text-black rounded-xl'>
+            <div className='h-full px-4'>
+              <div className='text-2xl font-bold text-black bg-white py-4 my-4 rounded-xl flex items-center justify-between'>
+                {editing ? (
+                  <input type="text" value={tempPetName} onChange={handleNameChange} onBlur={handleBlur} autoFocus className="text-2xl text-center mx-4 w-full animate-pulse" />
+                ) : (
+                  <>
+                    <span className="flex-1 text-center ml-16 mr-8 truncate">{petName}</span>
+                    <MdEdit onClick={toggleEditMode} className="mr-4 cursor-pointer" size={16} />
+                  </>
+                )}
               </div>
-              {showFood && (
-                <div className='z-40 absolute bottom-0 left-1/2 transform -translate-x-1/2 z-50 text-black'>
-                  <Image src={animations.food.sequence[frameIndex]} alt="Food" width={200} height={200} unoptimized={true} />
+              <div className='text-lg bg-white py-4 px-4 my-4 rounded-xl'>
+                level {currLevel}
+                <div className='w-full bg-white border-4 border-black mt-2'>
+                  <div className='bg-black/50 text-[10px] leading-none py-1 text-center text-white ease-in duration-200' style={{ width: `${levelProgress}%` }}>
+                    {/* {`${levelProgress}%`} */}
+                  </div>
                 </div>
-              )}
+              </div>
+              <div className='text-lg bg-white py-4 my-4 rounded-xl'>
+                favorite food: <span className='font-bold text-black/50'>{favoriteFood}</span>
+              </div>
+              <div className='text-lg bg-white py-10 my-4 rounded-xl cursor-not-allowed hover:bg-white/80 ease-in duration-100'>
+                customize
+              </div>
             </div>
+          </div>
+
+          <div className='h-full border-8 col-span-2 border-black rounded-xl'>
+
+            <div className='h-[90%] flex bg-white items-end justify-center'>
+
+              <div className='z-10 relative w-full h-full'>
+                <Image src={"/assets/backgrounds/" + currBg} layout="fill" />
+                <div className='z-20 absolute bottom-1 left-1/2 transform -translate-x-1/2'>
+                  <Image src={animations[currentAnimation].sequence[frameIndex]} alt="Pet" width={200} height={200} unoptimized={true} />
+                </div>
+                {showFood && (
+                  <div className='z-40 absolute bottom-0 left-1/2 transform -translate-x-1/2 z-50 text-black'>
+                    <Image src={animations.food.sequence[frameIndex]} alt="Food" width={200} height={200} unoptimized={true} />
+                  </div>
+                )}
+              </div>
+
+            </div>
+            <div className='w-full h-[10%] bg-black' />
+          </div>
+
+          <div className='h-full col-span-1 bg-white border-8 border-black mr-8 rounded-xl'>
 
           </div>
-          <div className='w-[50vw] h-[5vh] bg-black' />
+
         </div>
 
         <div className='fixed bottom-0 h-[15vh] w-[40vw] grid grid-cols-3 gap-2 border-8 border-black z-50'>
 
-          <div className='col-span-1'>
+          <div className='col-span-1 text-black'>
 
           </div>
 
-          <div onClick={feedPet} className={`${isFeeding ? 'bg-white/80 cursor-not-allowed' : 'bg-white hover:bg-white/80 cursor-pointer'} col-span-1 flex justify-center items-center text-black font-bold text-3xl`}>
+          <div onClick={feedPet} className={`${isFeeding ? 'bg-white/80 cursor-not-allowed' : 'bg-white hover:bg-white/80 cursor-pointer'} col-span-1 flex justify-center items-center text-black font-bold text-3xl rounded-xl ease-in duration-100`}>
             feed
           </div>
 
@@ -132,7 +213,7 @@ export default function Pet() {
             <div onClick={() => { if (!isFeeding) { changeFood(-1); } }} className={`${isFeeding ? 'cursor-not-allowed' : 'cursor-pointer'} flex justify-start pl-4 w-full`}>
               <FaCaretLeft className='text-white' size={20} />
             </div>
-            <div className='text-center'>
+            <div className='text-center ease-in'>
               <Image
                 src={`/assets/food/icons/${foodOptions[foodIndex]}.png`}
                 alt={foodOptions[foodIndex]}
